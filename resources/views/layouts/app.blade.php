0e69df37b5a244c298b23a99d93340be49ab5347
toto <test@test.com>
2021-10-22 20:51:21 +0300

    view of default layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Tasks</title>
+        <!-- CSS и JavaScript -->
+    </head>
+    <body>
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <!-- Содержимое Navbar -->
+        </nav>
+    </div>
+    @yield('content')
+    </body>
+</html>
